363ec0032785c62c9b2cf48e8da2c6a539ba4ecd
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-22 16:34:13 +0100

    3.2 checking

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,4 +4,9 @@ from pathlib import Path
 class Parser:
 
     extensions: List[str] = []
-    
+
+    def vaild_extension(self, extension):
+        if extension in self.extensions:
+            return True
+        else:
+            return False
