c3cc19713ecb12e9d65e9fce0f6f1fe3e7afbda5
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-22 22:57:16 +0100

    5.4-7 checking

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,7 +7,6 @@ from ssg.content import Content
 
 
 class Parser:
-
     extensions: List[str] = []
 
     def valid_extension(self, extension):
@@ -29,16 +28,14 @@ class Parser:
         shutil.copy2(path, dest / path.relative_to(source))
 
 
-
 class ResourceParser(Parser):
-
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
 
-class MarkdownParser(Parser):
 
+class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
     def parse(self, path: Path, source: Path, dest: Path):
@@ -46,3 +43,14 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+
+class RestructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(html["html_body"], path, dest)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
