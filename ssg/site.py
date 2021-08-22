088ffc77aaca8d90ad718eef45a7da7e84dbb3ce
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-22 14:10:17 +0100

    2.2 checking

---
+++ ssg/site.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path()
+        self.dest = Path()
+
+    def create_dir(self, path):
+        directory =  path
+
