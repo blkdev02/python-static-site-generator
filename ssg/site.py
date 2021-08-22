d06bf2b02a0e376a2da394ecaf6f8bb2c5604c91
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-22 14:42:28 +0100

    2.4 done

--- ssg/site.py
+++ ssg/site.py
@@ -8,5 +8,5 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest/path.relative_to(self.source)
-        directory = mkdir(parents=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
